4d9d5701e95672a98fa10562bbc001e3e7722688
Xuwznln <18435084+Xuwznln@users.noreply.github.com>
2025-10-24 02:56:07 +0800

    use ordering to convert identifier to idx

--- unilabos/ros/nodes/base_device_node.py
+++ unilabos/ros/nodes/base_device_node.py
@@ -593,10 +593,9 @@ class BaseROS2DeviceNode(Node, Generic[T]):
                     site = additional_add_params.get("site", None)
                     spec = inspect.signature(parent_resource.assign_child_resource)
                     if "spot" in spec.parameters:
-                        converter_func = getattr(plr_resource, "_parse_identifier_to_indices")
-                        if callable(converter_func):
-                            site = converter_func(site, 0)
-                            self.lab_logger().info(f"物料{plr_resource}转换挂载位置site: {site}")
+                        ordering_dict: Dict[str, Any] = getattr(plr_resource, "_ordering")
+                        if ordering_dict:
+                            site = list(ordering_dict.keys()).index(site)
                         additional_params["spot"] = site
                     old_parent = plr_resource.parent
                     if old_parent is not None:
